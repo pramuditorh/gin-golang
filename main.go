04da3283d670823a89dc5b43992028b5de2a3aec
Robby Hemawan Pramudito <pramuditorh@gmail.com>
2022-08-16 18:47:46 +0700

    Add delete functionality

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 // album represents data about a record album.
 type album struct {
-	ID     string  `json:"id"`
+	ID     int     `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
@@ -16,9 +17,9 @@ type album struct {
 
 // albums slice to seed record album data.
 var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: 1, Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func main() {
@@ -26,6 +27,7 @@ func main() {
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
+	router.DELETE("/albums/:id", deleteAlbumByID)
 
 	router.Run("0.0.0.0:8080")
 }
@@ -53,7 +55,11 @@ func postAlbums(c *gin.Context) {
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		panic(err)
+	}
 
 	// Loop through the list of albums, looking for
 	// an album whose ID value matches the parameter.
@@ -64,4 +70,26 @@ func getAlbumByID(c *gin.Context) {
 		}
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func deleteAlbumByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, a := range albums {
+		if a.ID == id {
+			deletedAlbum := RemoveIndex(albums, id-1)
+			albums = deletedAlbum
+			c.IndentedJSON(http.StatusOK, albums)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func RemoveIndex(s []album, i int) []album {
+	return append(s[:i], s[i+1:]...)
 }
